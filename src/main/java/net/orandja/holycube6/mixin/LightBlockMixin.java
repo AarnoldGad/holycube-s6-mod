3505f325f2013688e176b0205e38b20cc9cf4ef8
- The Aarnold - <mane.imation9@gmail.com>
2024-06-06 22:16:57 +0200

    Add creative bypass for light blocks

--- src/main/java/net/orandja/holycube6/mixin/LightBlockMixin.java
+++ src/main/java/net/orandja/holycube6/mixin/LightBlockMixin.java
@@ -2,10 +2,16 @@ package net.orandja.holycube6.mixin;
 
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.orandja.holycube6.modules.DebugWrench;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LightBlock.class)
 public abstract class LightBlockMixin extends Block implements Waterloggable {
@@ -21,5 +27,10 @@ public abstract class LightBlockMixin extends Block implements Waterloggable {
             }
         }
         super.onBlockBreakStart(state, world, pos, player);
+    }
+
+    @Redirect(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z"))
+    public boolean bypass_creative(PlayerEntity player, BlockState state, World world, BlockPos pos, PlayerEntity player1, BlockHitResult hit) {
+        return true;
     }
 }
